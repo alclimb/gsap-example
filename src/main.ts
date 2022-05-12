9d0fa14c4474f94ef6d8b254f168a8e8d1e281c7
hikaru <alclimb@users.noreply.github.com>
2022-05-12 11:44:39 +0900

    feat: アニメーションの追加

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,21 @@ import gsap from "gsap";
 import "./style.css";
 
 // ボタンクリックイベントの登録
-document.querySelector<HTMLDivElement>(`#button1`)!.addEventListener(`click`, () => {
+document.querySelector<HTMLDivElement>(`#button1`)!.addEventListener(`click`, async () => {
 
   // gsap: アニメーション実行
-  gsap.to(`#app > *`, {
-    duration: 0.3,  // アニメーション時間 (単位:秒)
-    opacity: 0.0,   // 透明度 (0.0 ～ 1.0)
-    x: -50,         // X位置 (単位: px)
-    stagger: 0.05,  // 対象が複数ある場合タイミングをずらす
+  await gsap.to(`#app > *`, {
+    duration: 0.3, // アニメーション時間 (単位:秒)
+    opacity: 0.0,  // 透明度 (0.0 ～ 1.0)
+    x: -50,        // X位置 (単位: px)
+    stagger: 0.05, // 複数の対象要素の場合にタイミングをずらす時間 (単位: 秒)
   });
-  
+
+  // gsap: アニメーション実行
+  await gsap.to(`#app`, {
+    duration: 0.3, // アニメーション時間 (単位:秒)
+    opacity: 0.0,  // 透明度 (0.0 ～ 1.0)
+    x: -50,        // X位置 (単位: px)
+  });
+
 });
