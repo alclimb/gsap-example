61493f9e5d1a44b064327e9b3248dbf336a2559b
hikaru <alclimb@users.noreply.github.com>
2022-05-17 17:31:48 +0900

    feat: リセットボタンを追加

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,7 @@ import gsap from "gsap";
 import "./style.css";
 
 // ボタンクリックイベントの登録
-document.querySelector<HTMLDivElement>(`#button1`)!.addEventListener(`click`, onClick);
-
-async function onClick() {
+document.querySelector<HTMLDivElement>(`#runButton`)!.addEventListener(`click`, async () => {
   // gsap: アニメーション実行
   await gsap.to(`#app > h1,p`, {
     duration: 0.3, // アニメーション時間 (単位:秒)
@@ -19,4 +17,22 @@ async function onClick() {
     opacity: 0.0,  // 透明度 (0.0 ～ 1.0)
     x: -50,        // X位置 (単位: px)
   });
-}
+});
+
+// ボタンクリックイベントの登録
+document.querySelector<HTMLDivElement>(`#resetButton`)!.addEventListener(`click`, async () => {
+  // gsap: アニメーション実行
+  await gsap.to(`#app`, {
+    duration: 0.3, // アニメーション時間 (単位:秒)
+    opacity: 1.0,  // 透明度 (0.0 ～ 1.0)
+    x: 0,        // X位置 (単位: px)
+  });
+
+  // gsap: アニメーション実行
+  await gsap.to(`#app > h1,p`, {
+    duration: 0.3, // アニメーション時間 (単位:秒)
+    opacity: 1.0,  // 透明度 (0.0 ～ 1.0)
+    x: 0,        // X位置 (単位: px)
+    stagger: 0.05, // 複数の対象要素の場合にタイミングをずらす時間 (単位: 秒)
+  });
+});
